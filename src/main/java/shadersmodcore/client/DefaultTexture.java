046606bff5cebf32b398656ed4a4b825a05f60c9
basdxz <tudurap.com@gmail.com>
2022-02-05 06:39:57 +0000

    Working on reflection

--- src/main/java/shadersmodcore/client/DefaultTexture.java
+++ src/main/java/shadersmodcore/client/DefaultTexture.java
@@ -11,6 +11,6 @@ public class DefaultTexture extends AbstractTexture {
     @Override
     public void loadTexture(IResourceManager resourcemanager) {
         int[] aint = ShadersTex.createAIntImage(1, -1);
-        ShadersTex.setupTexture(this.getMultiTexID(), aint, 1, 1, false, false);
+        ShadersTex.setupTexture(ReflectionHelper.getMultiTex(this), aint, 1, 1, false, false);
     }
 }
---
+++ src/main/java/shadersmodcore/client/ReflectionHelper.java
@@ -0,0 +1,12 @@
+package shadersmodcore.client;
+
+import net.minecraft.client.renderer.texture.AbstractTexture;
+
+public final class ReflectionHelper {
+    private ReflectionHelper() {
+    }
+
+    public static MultiTexID getMultiTex(AbstractTexture texture) {
+        return null;
+    }
+}
